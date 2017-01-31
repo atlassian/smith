9f0396b20721322e8028a65592b701d7ed4622be
Mikhail Mazurskiy <ash2kk@gmail.com>
2017-01-31 15:52:01 +1100

    Deep copy template from event

--- pkg/app/template_event_handler.go
+++ pkg/app/template_event_handler.go
@@ -1,6 +1,10 @@
 package app
 
-import "github.com/atlassian/smith"
+import (
+	"log"
+
+	"github.com/atlassian/smith"
+)
 
 type templateEventHandler struct {
 	processor Processor
@@ -14,11 +18,11 @@ func newTemplateEventHandler(processor Processor) *templateEventHandler {
 }
 
 func (h *templateEventHandler) OnAdd(obj interface{}) {
-	h.processor.Rebuild(obj.(*smith.Template))
+	h.handle(obj)
 }
 
 func (h *templateEventHandler) OnUpdate(oldObj, newObj interface{}) {
-	h.processor.Rebuild(newObj.(*smith.Template))
+	h.handle(newObj)
 }
 
 func (h *templateEventHandler) OnDelete(obj interface{}) {
@@ -26,4 +30,15 @@ func (h *templateEventHandler) OnDelete(obj interface{}) {
 	//		// "No direct deletion" convention? Use ObjectMeta.DeletionTimestamp like Namespace does?
 	//		// Somehow implement GC to do cleanup after template is deleted?
 	//		// Maybe store template in annotation on each resource to help reconstruct the dependency graph for GC?
+}
+
+func (h *templateEventHandler) handle(obj interface{}) {
+	in := obj.(*smith.Template)
+	out := &smith.Template{}
+
+	if err := smith.DeepCopy_Template(in, out); err != nil {
+		log.Printf("Failed to do deep copy of %#v: %v", in, err)
+		return
+	}
+	h.processor.Rebuild(out)
 }
