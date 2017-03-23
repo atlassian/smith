3143e8f5ea2e5db681984fb7a578ff2c542e35a1
Mikhail Mazurskiy <ash2kk@gmail.com>
2017-03-24 10:19:24 +1100

    Rename alias for consistency

--- pkg/readychecker/built_in_types.go
+++ pkg/readychecker/built_in_types.go
@@ -3,7 +3,7 @@ package readychecker
 import (
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	unstructured_conversion "k8s.io/apimachinery/pkg/conversion/unstructured"
-	extensions_v1beta1 "k8s.io/client-go/pkg/apis/extensions/v1beta1"
+	extensions "k8s.io/client-go/pkg/apis/extensions/v1beta1"
 )
 
 var converter unstructured_conversion.Converter = unstructured_conversion.NewConverter(false)
@@ -11,7 +11,7 @@ var converter unstructured_conversion.Converter = unstructured_conversion.NewCon
 // Works according to https://kubernetes.io/docs/user-guide/deployments/#the-status-of-a-deployment
 // and k8s.io/kubernetes/pkg/client/unversioned/conditions.go:120 DeploymentHasDesiredReplicas()
 func isDeploymentReady(obj *unstructured.Unstructured) (bool, error) {
-	var deployment extensions_v1beta1.Deployment
+	var deployment extensions.Deployment
 	if err := converter.FromUnstructured(obj.Object, &deployment); err != nil {
 		return false, err
 	}
