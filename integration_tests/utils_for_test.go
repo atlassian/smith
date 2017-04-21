f03a0416e910083c9035b68e2dfb49426bf326ae
Mikhail Mazurskiy <ash2kk@gmail.com>
2017-04-21 23:30:29 +1000

    Use await for better tests

--- integration_tests/utils_for_test.go
+++ integration_tests/utils_for_test.go
@@ -6,12 +6,18 @@ import (
 	"testing"
 
 	"github.com/atlassian/smith"
-
 	"github.com/atlassian/smith/examples/tprattribute"
+
 	"github.com/stretchr/testify/assert"
+	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/client-go/dynamic"
 	apiv1 "k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 )
 
 const (
@@ -37,4 +43,48 @@ func sleeperScheme() *runtime.Scheme {
 	scheme.AddUnversionedTypes(apiv1.SchemeGroupVersion, &metav1.Status{})
 	tprattribute.AddToScheme(scheme)
 	return scheme
+}
+
+func bundleInformer(bundleClient cache.Getter) cache.SharedIndexInformer {
+	return cache.NewSharedIndexInformer(
+		cache.NewListWatchFromClient(bundleClient, smith.BundleResourcePath, metav1.NamespaceAll, fields.Everything()),
+		&smith.Bundle{},
+		0,
+		cache.Indexers{})
+}
+
+func cleanupBundle(t *testing.T, bundleClient *rest.RESTClient, clients dynamic.ClientPool, bundleCreated *bool, bundle *smith.Bundle, bundleNamespace string) {
+	if !*bundleCreated {
+		return
+	}
+	t.Logf("Deleting bundle %s", bundle.Metadata.Name)
+	assert.NoError(t, bundleClient.Delete().
+		Namespace(bundleNamespace).
+		Resource(smith.BundleResourcePath).
+		Name(bundle.Metadata.Name).
+		Do().
+		Error())
+	for _, resource := range bundle.Spec.Resources {
+		t.Logf("Deleting resource %s", resource.Spec.GetName())
+		gv, err := schema.ParseGroupVersion(resource.Spec.GetAPIVersion())
+		if !assert.NoError(t, err) {
+			continue
+		}
+		client, err := clients.ClientForGroupVersionKind(gv.WithKind(resource.Spec.GetKind()))
+		if !assert.NoError(t, err) {
+			continue
+		}
+		plural, _ := meta.KindToResource(resource.Spec.GroupVersionKind())
+		assert.NoError(t, client.Resource(&metav1.APIResource{
+			Name:       plural.Resource,
+			Namespaced: true,
+			Kind:       resource.Spec.GetKind(),
+		}, bundleNamespace).Delete(resource.Spec.GetName(), nil))
+	}
+}
+
+func awaitBundleReady(obj runtime.Object) bool {
+	b := obj.(*smith.Bundle)
+	_, cond := b.GetCondition(smith.BundleReady)
+	return cond != nil && cond.Status == apiv1.ConditionTrue
 }
