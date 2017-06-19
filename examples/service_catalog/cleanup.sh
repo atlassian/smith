7f2f53b62a5e86b4d6a4416b05924891f00d31d7
Nail Islamov <nislamov@atlassian.com>
2017-06-19 15:19:57 +1000

    Fix service catalog bundle example

--- examples/service_catalog/cleanup.sh
+++ examples/service_catalog/cleanup.sh
@@ -3,6 +3,8 @@
 kubectl delete bundles/sampleapp
 kubectl delete podpresets/podpreset1
 kubectl delete deployments/deployment1
+kubectl delete service/service1
+kubectl delete ingress/ingress1
 kubectl delete instances/instance1 --context=service-catalog
 kubectl delete bindings/binding1 --context=service-catalog
 kubectl delete bindings/binding2 --context=service-catalog
