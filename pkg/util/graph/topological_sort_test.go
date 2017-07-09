b389ce2522511b972c4d66c3fecb98d347b30065
Mikhail Mazurskiy <ash2kk@gmail.com>
2017-07-10 09:25:43 +1000

    Rename variable

--- pkg/util/graph/topological_sort_test.go
+++ pkg/util/graph/topological_sort_test.go
@@ -9,101 +9,101 @@ import (
 
 func TestSort1(t *testing.T) {
 	t.Parallel()
-	graph := initGraph()
+	g := initGraph()
 
 	// a -> b -> c
-	require.NoError(t, graph.AddEdge("a", "b"))
-	require.NoError(t, graph.AddEdge("b", "c"))
+	require.NoError(t, g.AddEdge("a", "b"))
+	require.NoError(t, g.AddEdge("b", "c"))
 
-	assertSortResult(t, graph, []V{"c", "b", "a", "d"})
+	assertSortResult(t, g, []V{"c", "b", "a", "d"})
 }
 
 func TestSort2(t *testing.T) {
 	t.Parallel()
-	graph := initGraph()
+	g := initGraph()
 
 	// a -> c
 	// a -> b
 	// b -> c
-	require.NoError(t, graph.AddEdge("a", "c"))
-	require.NoError(t, graph.AddEdge("a", "b"))
-	require.NoError(t, graph.AddEdge("b", "c"))
+	require.NoError(t, g.AddEdge("a", "c"))
+	require.NoError(t, g.AddEdge("a", "b"))
+	require.NoError(t, g.AddEdge("b", "c"))
 
-	assertSortResult(t, graph, []V{"c", "b", "a", "d"})
+	assertSortResult(t, g, []V{"c", "b", "a", "d"})
 }
 
 func TestSort3(t *testing.T) {
 	t.Parallel()
-	graph := initGraph()
+	g := initGraph()
 
 	// a -> b
 	// a -> d
 	// d -> c
 	// c -> b
-	require.NoError(t, graph.AddEdge("a", "b"))
-	require.NoError(t, graph.AddEdge("a", "d"))
-	require.NoError(t, graph.AddEdge("d", "c"))
-	require.NoError(t, graph.AddEdge("c", "b"))
+	require.NoError(t, g.AddEdge("a", "b"))
+	require.NoError(t, g.AddEdge("a", "d"))
+	require.NoError(t, g.AddEdge("d", "c"))
+	require.NoError(t, g.AddEdge("c", "b"))
 
-	assertSortResult(t, graph, []V{"b", "c", "d", "a"})
+	assertSortResult(t, g, []V{"b", "c", "d", "a"})
 }
 
 func TestSortCycleError1(t *testing.T) {
 	t.Parallel()
-	graph := initGraph()
+	g := initGraph()
 
 	// a -> b
 	// b -> a
-	require.NoError(t, graph.AddEdge("a", "b"))
-	require.NoError(t, graph.AddEdge("b", "a"))
+	require.NoError(t, g.AddEdge("a", "b"))
+	require.NoError(t, g.AddEdge("b", "a"))
 
-	assertCycleDetection(t, graph)
+	assertCycleDetection(t, g)
 }
 
 func TestSortCycleError2(t *testing.T) {
 	t.Parallel()
-	graph := initGraph()
+	g := initGraph()
 
 	// a -> b
 	// b -> c
 	// c -> a
-	require.NoError(t, graph.AddEdge("a", "b"))
-	require.NoError(t, graph.AddEdge("b", "c"))
-	require.NoError(t, graph.AddEdge("c", "a"))
+	require.NoError(t, g.AddEdge("a", "b"))
+	require.NoError(t, g.AddEdge("b", "c"))
+	require.NoError(t, g.AddEdge("c", "a"))
 
-	assertCycleDetection(t, graph)
+	assertCycleDetection(t, g)
 }
 
 func TestSortCycleError3(t *testing.T) {
 	t.Parallel()
-	graph := initGraph()
+	g := initGraph()
 
 	// a -> b
 	// b -> c
 	// c -> b
-	require.NoError(t, graph.AddEdge("a", "b"))
-	require.NoError(t, graph.AddEdge("b", "c"))
-	require.NoError(t, graph.AddEdge("c", "b"))
+	require.NoError(t, g.AddEdge("a", "b"))
+	require.NoError(t, g.AddEdge("b", "c"))
+	require.NoError(t, g.AddEdge("c", "b"))
 
-	assertCycleDetection(t, graph)
+	assertCycleDetection(t, g)
 }
 
 func initGraph() *Graph {
-	graph := NewGraph(4)
-	graph.AddVertex("a", 1)
-	graph.AddVertex("b", 2)
-	graph.AddVertex("c", 3)
-	graph.AddVertex("d", 4)
-	return graph
+	g := NewGraph(4)
+	g.AddVertex("a", 1)
+	g.AddVertex("b", 2)
+	g.AddVertex("c", 3)
+	g.AddVertex("d", 4)
+	return g
 }
 
-func assertSortResult(t *testing.T, graph *Graph, expected []V) {
-	result, err := graph.TopologicalSort()
+func assertSortResult(t *testing.T, g *Graph, expected []V) {
+	result, err := g.TopologicalSort()
 	require.NoError(t, err)
 	assert.Equal(t, expected, result)
 }
 
-func assertCycleDetection(t *testing.T, graph *Graph) {
-	_, err := graph.TopologicalSort()
+func assertCycleDetection(t *testing.T, g *Graph) {
+	_, err := g.TopologicalSort()
 	require.Error(t, err)
 }
