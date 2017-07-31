4562c1c88cd1d9cf24b0cbe08d19498c768bab75
Mikhail Mazurskiy <126021+ash2k@users.noreply.github.com>
2017-07-31 17:21:41 +1000

    Refactor EnsureCrd method (#114)
    Two bugs are fixed:
    - defaults were not applied
    - status should be cleared before update to make sure we see update from controller,
    not a pre-existing status

--- pkg/resources/crd_helpers.go
+++ pkg/resources/crd_helpers.go
@@ -14,6 +14,11 @@ import (
 	api_errors "k8s.io/apimachinery/pkg/api/errors"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+)
+
+var (
+	crdGVK = apiext_v1b1.SchemeGroupVersion.WithKind("CustomResourceDefinition")
 )
 
 func BundleCrd() *apiext_v1b1.CustomResourceDefinition {
@@ -34,7 +39,6 @@ func BundleCrd() *apiext_v1b1.CustomResourceDefinition {
 }
 
 func EnsureCrdExists(ctx context.Context, scheme *runtime.Scheme, clientset crdClientset.Interface, store smith.ByNameStore, crd *apiext_v1b1.CustomResourceDefinition) error {
-	crdGVK := apiext_v1b1.SchemeGroupVersion.WithKind("CustomResourceDefinition")
 	for {
 		obj, exists, err := store.Get(crdGVK, meta_v1.NamespaceNone, crd.Name)
 		if err != nil {
@@ -42,55 +46,75 @@ func EnsureCrdExists(ctx context.Context, scheme *runtime.Scheme, clientset crdC
 		}
 		if exists {
 			o := obj.(*apiext_v1b1.CustomResourceDefinition)
-			if !IsEqualCrd(crd, o, scheme) {
-				log.Printf("Updating CustomResourceDefinition %s", crd.Name)
-				o.Spec = crd.Spec
-				o.Annotations = crd.Annotations
-				o.Labels = crd.Labels
-				_, err = clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Update(o) // This is a CAS
-				if err != nil {
-					if !api_errors.IsConflict(err) {
-						return fmt.Errorf("failed to update CustomResourceDefinition %s: %v", crd.Name, err)
-					}
-					log.Printf("Conflict updating CustomResourceDefinition %s", crd.Name)
-					// wait for store to pick up the object and re-iterate
-					if err := util.Sleep(ctx, 1*time.Second); err != nil {
-						return err
-					}
-					continue
-				}
+			if IsEqualCrd(crd, o, scheme) {
+				return nil
 			}
+			log.Printf("Updating CustomResourceDefinition %s", crd.Name)
+			o.Spec = crd.Spec
+			o.Annotations = crd.Annotations
+			o.Labels = crd.Labels
+			// TODO this is only necessary because there is no support for generation/observedGeneration at the moment
+			o.Status = apiext_v1b1.CustomResourceDefinitionStatus{}
+			_, err = clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Update(o) // This is a CAS
+			if err == nil {
+				log.Printf("CustomResourceDefinition %s updated, waiting for it to become established", crd.Name)
+				return WaitForCrdToBecomeEstablished(ctx, store, crd)
+			}
+			if !api_errors.IsConflict(err) {
+				return fmt.Errorf("failed to update CustomResourceDefinition %s: %v", crd.Name, err)
+			}
+			log.Printf("Conflict updating CustomResourceDefinition %s, retrying", crd.Name)
 		} else {
 			log.Printf("Creating CustomResourceDefinition %s", crd.Name)
 			_, err := clientset.ApiextensionsV1beta1().CustomResourceDefinitions().Create(crd)
-			if err != nil {
-				if !api_errors.IsAlreadyExists(err) {
-					return fmt.Errorf("failed to create %s CustomResourceDefinition: %v", crd.Name, err)
-				}
-				log.Printf("CustomResourceDefinition %s was created concurrently", crd.Name)
-				// wait for store to pick up the object and re-iterate
-				if err := util.Sleep(ctx, 1*time.Second); err != nil {
-					return err
-				}
-				continue
+			if err == nil {
+				log.Printf("CustomResourceDefinition %s created, waiting for it to become established", crd.Name)
+				return WaitForCrdToBecomeEstablished(ctx, store, crd)
 			}
-			log.Printf("CustomResourceDefinition %s created", crd.Name)
-			// TODO It takes a while for k8s to add a new rest endpoint. Polling?
-			if err := util.Sleep(ctx, 15*time.Second); err != nil {
-				return err
+			if !api_errors.IsAlreadyExists(err) {
+				return fmt.Errorf("failed to create %s CustomResourceDefinition: %v", crd.Name, err)
+			}
+			log.Printf("CustomResourceDefinition %s was created concurrently", crd.Name)
+		}
+		// wait for store to pick up the object and re-iterate
+		if err = util.Sleep(ctx, time.Second); err != nil {
+			return err
+		}
+	}
+}
+
+func WaitForCrdToBecomeEstablished(ctx context.Context, store smith.ByNameStore, crd *apiext_v1b1.CustomResourceDefinition) error {
+	return wait.PollUntil(100*time.Millisecond, func() (done bool, err error) {
+		obj, exists, err := store.Get(crdGVK, meta_v1.NamespaceNone, crd.Name)
+		if err != nil || !exists {
+			return false, err
+		}
+		// TODO check generation/observedGeneration when supported
+		established := false
+		for _, cond := range obj.(*apiext_v1b1.CustomResourceDefinition).Status.Conditions {
+			switch cond.Type {
+			case apiext_v1b1.Established:
+				if cond.Status == apiext_v1b1.ConditionTrue {
+					established = true
+				}
+			case apiext_v1b1.NamesAccepted:
+				if cond.Status == apiext_v1b1.ConditionFalse {
+					return false, fmt.Errorf("failed to create CRD %s: name conflict: %s", crd.Name, cond.Reason)
+				}
 			}
 		}
-		break
-	}
-	return nil
+		return established, nil
+	}, ctx.Done())
 }
 
 func IsEqualCrd(a, b *apiext_v1b1.CustomResourceDefinition, scheme *runtime.Scheme) bool {
 	aCopy := *a
 	bCopy := *b
+	a = &aCopy
+	b = &bCopy
 
-	scheme.Default(&aCopy)
-	scheme.Default(&bCopy)
+	scheme.Default(a)
+	scheme.Default(b)
 
 	// Ignoring labels and annotations for now
 	as := a.Spec
