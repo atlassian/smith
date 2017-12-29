248ce705f2463886f4b2e966dbb693e7241ecd1d
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2017-12-29 15:02:52 +1100

    OpenAPI schema for Bundle CRD

--- pkg/resources/crd_helpers.go
+++ pkg/resources/crd_helpers.go
@@ -3,6 +3,7 @@ package resources
 import (
 	"context"
 	"log"
+	"reflect"
 	"time"
 
 	"github.com/atlassian/smith/pkg/apis/smith"
@@ -20,6 +21,11 @@ import (
 )
 
 func BundleCrd() *apiext_v1b1.CustomResourceDefinition {
+	// Schema is based on:
+	// https://github.com/kubernetes/community/blob/master/contributors/design-proposals/architecture/identifiers.md
+	// https://github.com/kubernetes/community/blob/master/contributors/devel/api-conventions.md
+	// https://github.com/kubernetes/community/blob/master/contributors/design-proposals/architecture/namespaces.md
+	// https://github.com/kubernetes/kubernetes/tree/master/api/openapi-spec
 	return &apiext_v1b1.CustomResourceDefinition{
 		ObjectMeta: meta_v1.ObjectMeta{
 			Name: smith_v1.BundleResourceName,
@@ -32,8 +38,223 @@ func BundleCrd() *apiext_v1b1.CustomResourceDefinition {
 				Singular: smith_v1.BundleResourceSingular,
 				Kind:     smith_v1.BundleResourceKind,
 			},
+			Validation: &apiext_v1b1.CustomResourceValidation{
+				OpenAPIV3Schema: &apiext_v1b1.JSONSchemaProps{
+					Properties: map[string]apiext_v1b1.JSONSchemaProps{
+						"spec": {
+							Type:     "object",
+							Required: []string{"resources"},
+							Properties: map[string]apiext_v1b1.JSONSchemaProps{
+								"resources": {
+									Type: "array",
+									Items: &apiext_v1b1.JSONSchemaPropsOrArray{
+										Schema: &apiext_v1b1.JSONSchemaProps{
+											Ref: strPtr("#/definitions/resource"),
+										},
+									},
+								},
+							},
+						},
+					},
+					Definitions: apiext_v1b1.JSONSchemaDefinitions{
+						"DNS_SUBDOMAIN": {
+							Type:      "string",
+							MinLength: int64ptr(1),
+							MaxLength: int64ptr(253),
+							Pattern:   `^[a-z0-9]([-a-z0-9]*[a-z0-9])?(\.[a-z0-9]([-a-z0-9]*[a-z0-9])?)*$`,
+						},
+						"resourceName": {
+							Ref:         strPtr("#/definitions/DNS_SUBDOMAIN"),
+							Description: "ResourceName is a reference to another Resource in the same bundle",
+						},
+						"resource": {
+							Description: "Resource describes an object that should be provisioned",
+							Type:        "object",
+							Required:    []string{"name", "spec"},
+							Properties: map[string]apiext_v1b1.JSONSchemaProps{
+								"name": {
+									Ref: strPtr("#/definitions/resourceName"),
+								},
+								"dependsOn": {
+									Type: "array",
+									Items: &apiext_v1b1.JSONSchemaPropsOrArray{
+										Schema: &apiext_v1b1.JSONSchemaProps{
+											Ref: strPtr("#/definitions/resourceName"),
+										},
+									},
+								},
+								"spec": {
+									Type: "object",
+									OneOf: []apiext_v1b1.JSONSchemaProps{
+										{
+											Required: []string{"object"},
+											Properties: map[string]apiext_v1b1.JSONSchemaProps{
+												"object": {
+													Ref: strPtr("#/definitions/objectSpec"),
+												},
+											},
+										},
+										{
+											Required: []string{"plugin"},
+											Properties: map[string]apiext_v1b1.JSONSchemaProps{
+												"plugin": {
+													Ref: strPtr("#/definitions/pluginSpec"),
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+						"objectSpec": {
+							Ref:         strPtr("#/definitions/typeMeta"),
+							Description: "Schema for a resource that describes an object",
+							Type:        "object",
+							Required:    []string{"metadata"},
+							Properties: map[string]apiext_v1b1.JSONSchemaProps{
+								"metadata": {
+									Ref: strPtr("#/definitions/objectMeta"),
+								},
+							},
+						},
+						"pluginSpec": {
+							Description: "Schema for a resource that describes a plugin",
+							Type:        "object",
+							Required:    []string{"name", "objectName"},
+							Properties: map[string]apiext_v1b1.JSONSchemaProps{
+								"name": {
+									Ref: strPtr("#/definitions/DNS_SUBDOMAIN"),
+								},
+								"objectName": {
+									Ref: strPtr("#/definitions/DNS_SUBDOMAIN"),
+								},
+								"spec": {
+									Type: "object",
+								},
+							},
+						},
+						"apiVersion": {
+							Type:      "string",
+							MinLength: int64ptr(1),
+						},
+						"kind": {
+							Type:      "string",
+							MinLength: int64ptr(1),
+						},
+						"typeMeta": {
+							Description: "Schema for TypeMeta",
+							Type:        "object",
+							Required:    []string{"kind", "apiVersion"},
+							Properties: map[string]apiext_v1b1.JSONSchemaProps{
+								"kind": {
+									Ref: strPtr("#/definitions/kind"),
+								},
+								"apiVersion": {
+									Ref: strPtr("#/definitions/apiVersion"),
+								},
+							},
+						},
+						"objectMeta": {
+							Description: "Schema for some fields of ObjectMeta",
+							Type:        "object",
+							Properties: map[string]apiext_v1b1.JSONSchemaProps{
+								"name": {
+									Ref: strPtr("#/definitions/DNS_SUBDOMAIN"),
+								},
+								"labels": {
+									Type: "object",
+									AdditionalProperties: &apiext_v1b1.JSONSchemaPropsOrBool{
+										Schema: &apiext_v1b1.JSONSchemaProps{
+											Type:      "string",
+											MaxLength: int64ptr(63),
+											Pattern:   "^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])?$",
+										},
+									},
+								},
+								"annotations": {
+									Type: "object",
+									AdditionalProperties: &apiext_v1b1.JSONSchemaPropsOrBool{
+										Schema: &apiext_v1b1.JSONSchemaProps{
+											Type: "string",
+										},
+									},
+								},
+								"ownerReference": {
+									Type:     "object",
+									Required: []string{"apiVersion", "kind", "name"},
+									Properties: map[string]apiext_v1b1.JSONSchemaProps{
+										"kind": {
+											Ref: strPtr("#/definitions/kind"),
+										},
+										"apiVersion": {
+											Ref: strPtr("#/definitions/apiVersion"),
+										},
+										"name": {
+											Ref: strPtr("#/definitions/DNS_SUBDOMAIN"),
+										},
+										"blockOwnerDeletion": {
+											Type: "boolean",
+										},
+										"controller": {
+											Type: "boolean",
+										},
+									},
+								},
+								"ownerReferences": {
+									Type: "array",
+									Items: &apiext_v1b1.JSONSchemaPropsOrArray{
+										Schema: &apiext_v1b1.JSONSchemaProps{
+											Ref: strPtr("#/definitions/ownerReference"),
+										},
+									},
+								},
+								"initializer": {
+									Type:     "object",
+									Required: []string{"name"},
+									Properties: map[string]apiext_v1b1.JSONSchemaProps{
+										"name": {
+											Type: "string",
+										},
+									},
+								},
+								"initializers": {
+									Type:     "object",
+									Required: []string{"pending"},
+									Properties: map[string]apiext_v1b1.JSONSchemaProps{
+										"pending": {
+											Type: "array",
+											Items: &apiext_v1b1.JSONSchemaPropsOrArray{
+												Schema: &apiext_v1b1.JSONSchemaProps{
+													Ref: strPtr("#/definitions/initializer"),
+												},
+											},
+										},
+									},
+								},
+								"finalizers": {
+									Type: "array",
+									Items: &apiext_v1b1.JSONSchemaPropsOrArray{
+										Schema: &apiext_v1b1.JSONSchemaProps{
+											Type:      "string",
+											MinLength: int64ptr(1),
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
+}
+
+func strPtr(str string) *string {
+	return &str
+}
+
+func int64ptr(val int64) *int64 {
+	return &val
 }
 
 func EnsureCrdExistsAndIsEstablished(ctx context.Context, defaulter runtime.ObjectDefaulter, clientset crdClientset.Interface, crdLister apiext_lst_v1b1.CustomResourceDefinitionLister, crd *apiext_v1b1.CustomResourceDefinition) error {
@@ -68,6 +289,7 @@ func EnsureCrdExists(ctx context.Context, defaulter runtime.ObjectDefaulter, cli
 				return nil
 			}
 			log.Printf("Updating CustomResourceDefinition %s", crd.Name)
+			obj = obj.DeepCopy()
 			obj.Spec = crd.Spec
 			obj.Annotations = crd.Annotations
 			obj.Labels = crd.Labels
@@ -146,13 +368,12 @@ func IsEqualCrd(a, b *apiext_v1b1.CustomResourceDefinition, defaulter runtime.Ob
 	bs := b.Spec
 	return as.Group == bs.Group &&
 		as.Version == bs.Version &&
-		isEqualCrdNames(a, b) &&
-		a.Spec.Scope == b.Spec.Scope
+		isEqualCrdNames(as.Names, bs.Names) &&
+		a.Spec.Scope == b.Spec.Scope &&
+		isEqualValidation(as.Validation, bs.Validation)
 }
 
-func isEqualCrdNames(a, b *apiext_v1b1.CustomResourceDefinition) bool {
-	n1 := a.Spec.Names
-	n2 := b.Spec.Names
+func isEqualCrdNames(n1, n2 apiext_v1b1.CustomResourceDefinitionNames) bool {
 	return n1.Plural == n2.Plural &&
 		n1.Singular == n2.Singular &&
 		isEqualShortNames(n1.ShortNames, n2.ShortNames) &&
@@ -172,4 +393,8 @@ func isEqualShortNames(a, b []string) bool {
 	}
 
 	return true
+}
+
+func isEqualValidation(av, bv *apiext_v1b1.CustomResourceValidation) bool {
+	return reflect.DeepEqual(av, bv)
 }
