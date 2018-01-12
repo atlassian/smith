964bcda27e9a330eb59fd0fca895df0c7f6adaf4
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-01-12 21:04:04 +1100

    Fix secret data merging

--- pkg/controller/controller_test.go
+++ pkg/controller/controller_test.go
@@ -95,7 +95,8 @@ const (
 	si1    = "si1"
 	si1uid = "si1-uid"
 
-	s1 = "s1"
+	s1    = "s1"
+	s1uid = "s1-uid"
 
 	m1 = "m1"
 
@@ -338,6 +339,7 @@ func TestController(t *testing.T) {
 					ObjectMeta: meta_v1.ObjectMeta{
 						Name:      s1,
 						Namespace: testNamespace,
+						UID:       s1uid,
 						OwnerReferences: []meta_v1.OwnerReference{
 							{
 								APIVersion:         sc_v1b1.SchemeGroupVersion.String(),
@@ -699,6 +701,99 @@ func TestController(t *testing.T) {
 				resCond := smith_testing.AssertResourceCondition(t, updateBundle, "map-needs-update", smith_v1.ResourceError, smith_v1.ConditionTrue)
 				assert.Equal(t, smith_v1.ResourceReasonTerminalError, resCond.Reason)
 				assert.Equal(t, "specification of the created/updated object does not match the desired spec", resCond.Message)
+			},
+		},
+		"Secret's keys should not be merged": &testCase{
+			mainClientObjects: []runtime.Object{
+				&core_v1.Secret{
+					ObjectMeta: meta_v1.ObjectMeta{
+						Name:      s1,
+						Namespace: testNamespace,
+						UID:       s1uid,
+						OwnerReferences: []meta_v1.OwnerReference{
+							{
+								APIVersion:         smith_v1.BundleResourceGroupVersion,
+								Kind:               smith_v1.BundleResourceKind,
+								Name:               bundle1,
+								UID:                bundle1uid,
+								Controller:         &tr,
+								BlockOwnerDeletion: &tr,
+							},
+						},
+					},
+					Data: map[string][]byte{
+						"data_actual": []byte("bla0"),
+					},
+					Type: core_v1.SecretTypeOpaque,
+				},
+			},
+			bundle: &smith_v1.Bundle{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name:      bundle1,
+					Namespace: testNamespace,
+					UID:       bundle1uid,
+				},
+				Spec: smith_v1.BundleSpec{
+					Resources: []smith_v1.Resource{
+						{
+							Name: resSi1,
+							Spec: smith_v1.ResourceSpec{
+								Object: &core_v1.Secret{
+									TypeMeta: meta_v1.TypeMeta{
+										Kind:       "Secret",
+										APIVersion: core_v1.SchemeGroupVersion.String(),
+									},
+									ObjectMeta: meta_v1.ObjectMeta{
+										Name: s1,
+									},
+									StringData: map[string]string{
+										"data_spec2": "bla2",
+									},
+									Type: core_v1.SecretTypeOpaque,
+								},
+							},
+						},
+					},
+				},
+			},
+			namespace:       testNamespace,
+			expectedActions: sets.NewString("PUT=/api/v1/namespaces/" + testNamespace + "/secrets/" + s1),
+			testHandler: fakeActionHandler{
+				response: map[path]fakeResponse{
+					{
+						method: "PUT",
+						path:   "/api/v1/namespaces/" + testNamespace + "/secrets/" + s1,
+					}: {
+						statusCode: http.StatusOK,
+						content: []byte(`{
+							"apiVersion": "v1",
+							"kind": "Secret",
+							"metadata": {
+								"name": "` + s1 + `",
+								"namespace": "` + testNamespace + `",
+								"uid": "` + s1uid + `",
+								"labels": {
+									"` + smith.BundleNameLabel + `": "` + bundle1 + `"
+								},
+								"creationTimestamp": null,
+								"ownerReferences": [
+									{
+										"apiVersion": "` + smith_v1.BundleResourceGroupVersion + `",
+										"kind": "` + smith_v1.BundleResourceKind + `",
+										"name": "` + bundle1 + `",
+										"uid": "` + bundle1uid + `",
+										"controller": true,
+										"blockOwnerDeletion": true
+									}
+								]
+							},
+							"data": {
+								"data_spec2": "YmxhMg=="
+							},
+							"type": "Opaque"
+						}`),
+					},
+				},
 			},
 		},
 	}
