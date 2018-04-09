1b92df8a2347d9248d6c4b9cc3f27d59c3aa109f
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-04-09 15:08:21 +1000

    Remove PodPreset remnant

--- pkg/resources/apitypes/scheme.go
+++ pkg/resources/apitypes/scheme.go
@@ -7,7 +7,6 @@ import (
 	apps_v1 "k8s.io/api/apps/v1"
 	core_v1 "k8s.io/api/core/v1"
 	ext_v1b1 "k8s.io/api/extensions/v1beta1"
-	settings_v1a1 "k8s.io/api/settings/v1alpha1"
 	apiext_v1b1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1beta1"
 	meta_v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -20,7 +19,6 @@ func FullScheme(serviceCatalog bool) (*runtime.Scheme, error) {
 	sb.Register(ext_v1b1.SchemeBuilder...)
 	sb.Register(core_v1.SchemeBuilder...)
 	sb.Register(apps_v1.SchemeBuilder...)
-	sb.Register(settings_v1a1.SchemeBuilder...)
 	sb.Register(apiext_v1b1.SchemeBuilder...)
 	if serviceCatalog {
 		sb.Register(sc_v1b1.SchemeBuilder...)
