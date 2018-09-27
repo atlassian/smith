65f888a86869ae36ced570c144dfa4666f42da45
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-09-27 15:24:16 +0300

    Cleanup Bundle CRD when printing it

--- cmd/crd/main.go
+++ cmd/crd/main.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 
 	"github.com/atlassian/smith/pkg/resources"
-	"github.com/ghodss/yaml"
-	"github.com/pkg/errors"
 )
 
 func main() {
@@ -23,26 +20,5 @@ func innerMain() error {
 	printBundle := flag.String("print-bundle", "yaml", "Print Bundle CRD and exit (specify format: json or yaml)")
 	flag.Parse()
 
-	switch *printBundle {
-	case "json":
-		enc := json.NewEncoder(os.Stdout)
-		enc.SetIndent("", "  ")
-		enc.SetEscapeHTML(false)
-		err := enc.Encode(resources.BundleCrd())
-		if err != nil {
-			return errors.Wrap(err, "failed to marshal Bundle CRD into JSON")
-		}
-	case "yaml":
-		data, err := yaml.Marshal(resources.BundleCrd())
-		if err != nil {
-			return errors.Wrap(err, "failed to marshal Bundle CRD into YAML")
-		}
-		_, err = os.Stdout.Write(data)
-		if err != nil {
-			return errors.Wrap(err, "failed to write Bundle CRD YAML to stdout")
-		}
-	default:
-		return errors.Errorf("unsupported Bundle CRD output format %q", *printBundle)
-	}
-	return nil
+	return resources.PrintCleanedObject(os.Stdout, *printBundle, resources.BundleCrd())
 }
