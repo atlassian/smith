a22dae6ec67b9cdf4acbe6c2d0cf144e12d439ea
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-11-04 12:23:57 +1100

    Merge speccheck and cleanup; func -> ObjectProcessor interface

--- pkg/speccheck/types.go
+++ pkg/speccheck/types.go
@@ -3,8 +3,14 @@ package speccheck
 import (
 	"go.uber.org/zap"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
 )
 
-type SpecCleaner interface {
-	Cleanup(logger *zap.Logger, spec, actual *unstructured.Unstructured) (updatedSpec *unstructured.Unstructured, err error)
+type ObjectProcessor interface {
+	ApplySpec(ctx *Context, spec, actual *unstructured.Unstructured) (runtime.Object /*updatedSpec*/, error)
+}
+
+// Context includes objects used by different cleanup functions
+type Context struct {
+	Logger *zap.Logger
 }
