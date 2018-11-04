e3c678b20d7344734f8d1701e1d36f627ee8ce61
Mikhail Mazurskiy <mmazurskiy@atlassian.com>
2018-11-04 12:50:25 +1100

    Split processing code into small files

---
+++ pkg/speccheck/builtin/known_types.go
@@ -0,0 +1,22 @@
+package builtin
+
+import (
+	"github.com/atlassian/smith/pkg/speccheck"
+	sc_v1b1 "github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
+	apps_v1 "k8s.io/api/apps/v1"
+	core_v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+)
+
+var (
+	MainKnownTypes = map[schema.GroupKind]speccheck.ObjectProcessor{
+		{Group: apps_v1.GroupName, Kind: "Deployment"}: deployment{},
+		{Group: core_v1.GroupName, Kind: "Service"}:    service{},
+		{Group: core_v1.GroupName, Kind: "Secret"}:     secret{},
+	}
+
+	ServiceCatalogKnownTypes = map[schema.GroupKind]speccheck.ObjectProcessor{
+		{Group: sc_v1b1.GroupName, Kind: "ServiceBinding"}:  serviceBinding{},
+		{Group: sc_v1b1.GroupName, Kind: "ServiceInstance"}: serviceInstance{},
+	}
+)
---
+++ pkg/speccheck/builtin/process_util.go
@@ -0,0 +1,53 @@
+package builtin
+
+import (
+	"reflect"
+
+	sc_v1b1 "github.com/kubernetes-incubator/service-catalog/pkg/apis/servicecatalog/v1beta1"
+	"github.com/pkg/errors"
+	apps_v1 "k8s.io/api/apps/v1"
+	core_v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+)
+
+var (
+	appsV1Scheme = runtime.NewScheme()
+	scV1B1Scheme = runtime.NewScheme()
+	coreV1Scheme = runtime.NewScheme()
+)
+
+func init() {
+	utilruntime.Must(apps_v1.SchemeBuilder.AddToScheme(appsV1Scheme))
+	utilruntime.Must(sc_v1b1.SchemeBuilder.AddToScheme(scV1B1Scheme))
+	utilruntime.Must(core_v1.SchemeBuilder.AddToScheme(coreV1Scheme))
+}
+
+// setFieldsFromActual mutates the target with fields from the instantiated object,
+// iff that field was not set in the original object.
+func setEmptyFieldsFromActual(requested, actual interface{}, fields ...string) error {
+	requestedValue := reflect.ValueOf(requested).Elem()
+	actualValue := reflect.ValueOf(actual).Elem()
+
+	if requestedValue.Type() != actualValue.Type() {
+		return errors.Errorf("attempted to set fields from different types: %q from %q",
+			requestedValue, actualValue)
+	}
+
+	for _, field := range fields {
+		requestedField := requestedValue.FieldByName(field)
+		if !requestedField.IsValid() {
+			return errors.Errorf("no such field %q to cleanup", field)
+		}
+		actualField := actualValue.FieldByName(field)
+		if !actualField.IsValid() {
+			return errors.Errorf("no such field %q to cleanup", field)
+		}
+
+		if reflect.DeepEqual(requestedField.Interface(), reflect.Zero(requestedField.Type()).Interface()) {
+			requestedField.Set(actualField)
+		}
+	}
+
+	return nil
+}
