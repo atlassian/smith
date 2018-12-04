44e55b678f3bad7e16f6a565559c8784c1338a06
Yiyang Chen <ychen@atlassian.com>
2018-12-04 10:18:12 -0800

    Actually Invalid not bad request

--- pkg/controller/bundlec/resource_sync_task.go
+++ pkg/controller/bundlec/resource_sync_task.go
@@ -151,7 +151,7 @@ func (st *resourceSyncTask) processResource(res *smith_v1.Resource) resourceInfo
 	// Create or update resource
 	resUpdated, retriable, err := st.createOrUpdate(spec, actual)
 	if err != nil {
-		if api_errors.IsBadRequest(err) {
+		if api_errors.IsInvalid(err) {
 			return resourceInfo{
 				actual: resUpdated,
 				status: resourceStatusError{
